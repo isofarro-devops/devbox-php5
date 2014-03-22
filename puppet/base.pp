b9c33ed1938769c9ce2e971ab49693db3b8ec808
Mike Davies <isofarro2@gmail.com>
2014-03-22 12:39:57 +0000

    trim out project specific configuration, this becomes a plain baseline.

---
+++ puppet/base.pp
@@ -0,0 +1,38 @@
+#
+# Global defaults/config
+#
+Exec {
+    path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
+}
+
+# Project variables
+$projectName   = "devbox-php5"
+$projectDir    = "/home/vagrant/Projects/${projectName}"
+$projectDomain = "devbox-php.dev"
+
+
+#
+# Modules: what does your project need?
+#
+
+# Base Ubuntu packages and configuration
+include baseconfig
+
+# Mysql client and server
+include mysql
+
+# Nginx web server
+include nginx
+
+# PHP install: CLI and FPM
+include php
+
+# Beanstalk message queue (optional)
+#include beanstalk
+
+
+
+#
+# Project specific config
+#
+
