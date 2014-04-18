a84387978f251d7e8c4cad6ad137f0ff5c1e908e
Mike Davies <isofarro2@gmail.com>
2014-04-18 11:28:57 +0100

    break php composer out into a separate module

---
+++ puppet/modules/php/manifests/composer.pp
@@ -0,0 +1,17 @@
+class php::composer {
+
+    exec { 'download-composer':
+        creates => '/tmp/composer.phar',
+        command => 'curl -sS https://getcomposer.org/installer | php -- --install-dir=/tmp',
+        user    => 'root',
+        require => Package['php5-baseline'],
+    }
+
+    exec { 'install-composer':
+        creates => '/usr/local/bin/composer',
+        command => 'cp /tmp/composer.phar /usr/local/bin/composer && chmod +x /usr/local/bin/composer',
+        user    => 'root',
+        require => Exec['download-composer'],
+    }
+
+}
--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -68,17 +68,5 @@ class php {
 		require => Exec['download-phpunit'],
 	}
 
-	exec { 'download-composer':
-		creates => '/tmp/composer.phar',
-		command => 'curl -sS https://getcomposer.org/installer | php -- --install-dir=/tmp',
-		user    => 'root',
-		require => Package['php5-baseline'],
-	}
-
-	exec { 'install-composer':
-		creates => '/usr/local/bin/composer',
-		command => 'cp /tmp/composer.phar /usr/local/bin/composer && chmod +x /usr/local/bin/composer',
-		user    => 'root',
-		require => Exec['download-composer'],
-	}
+	include php::composer
 }
