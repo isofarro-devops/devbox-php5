84233538195b25a2a7f4eae218ec1428343529c8
Mike Davies <isofarro2@gmail.com>
2015-02-10 15:14:14 +0000

    Fix packages to work with puppet > 3.4.0

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -2,19 +2,19 @@
 # Nginx
 #
 class nginx {
-	package { 'nginx':
-		name   => 'nginx',
-		ensure => installed,
-		require => Package['core-packages'],
-	}
+    package { 'nginx':
+        name   => 'nginx',
+        ensure => installed,
+        require => Class['baseconfig'],
+    }
 
-	service { 'nginx':
-		ensure     => running,
-		enable     => true,
-		hasrestart => true,
-		hasstatus  => true,
-		require    => [
-			Package['nginx'],
-		],
-	}
+    service { 'nginx':
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => [
+            Package['nginx'],
+        ],
+    }
 }
