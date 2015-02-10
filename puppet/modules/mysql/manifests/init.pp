76aed6a20f1799964ae87ba1f00b6a1d01a8b857
Mike Davies <isofarro2@gmail.com>
2015-02-10 15:13:53 +0000

    Fix packages to work with puppet > 3.4.0

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -2,54 +2,54 @@
 # Mysql
 #
 class mysql {
-	package { 'mysql':
-		name => [
-			'mysql-client',
-			'mysql-server'
-		],
-		ensure => installed,
-		require => Package['core-packages'],
-	}
+    package { 'mysql-server':
+        ensure => installed,
+        require => Class['baseconfig'],
+    }
+
+    package { 'mysql-client':
+        ensure => installed,
+        require => Class['baseconfig'],
+    }
+
+    file { '/var/lib/mysql/my.cnf':
+        owner   => 'mysql',
+        group   => 'mysql',
+        source  => 'puppet:///modules/mysql/my.cnf',
+        notify  => Service['mysql'],
+        require => Package['mysql-server']
+    }
 
 
-	file { '/var/lib/mysql/my.cnf':
-		owner   => 'mysql',
-		group   => 'mysql',
-		source  => 'puppet:///modules/mysql/my.cnf',
-		notify  => Service['mysql'],
-		require => Package['mysql']
-	}
+    file { '/etc/my.cnf':
+        require => File['/var/lib/mysql/my.cnf'],
+        ensure  => '/var/lib/mysql/my.cnf'
+    }
 
 
-	file { '/etc/my.cnf':
-		require => File['/var/lib/mysql/my.cnf'],
-		ensure  => '/var/lib/mysql/my.cnf'
-	}
+    exec { 'set-mysql-password':
+        unless => 'mysqladmin -uroot -prootpwd status',
+        command => 'mysqladmin -uroot password rootpwd',
+        path   => ['/bin', '/usr/bin'],
+        require => [
+            Package['mysql-server'],
+        ]
+    }
 
+    exec { 'create-vagrant-user':
+        command => "mysql -uroot -prootpwd -e \"GRANT ALL PRIVILEGES ON *.* TO webdev@localhost IDENTIFIED BY 'webdev';\"",
+        path    => ['/usr/bin'],
+        require => Exec['set-mysql-password'],
+    }
 
-	exec { 'set-mysql-password':
-		unless => 'mysqladmin -uroot -prootpwd status',
-		command => 'mysqladmin -uroot password rootpwd',
-		path   => ['/bin', '/usr/bin'],
-		require => [
-			Package['mysql'],
-		]
-	}
+    service { 'mysql':
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => [
+            Package['mysql-server'],
+        ],
 
-	exec { 'create-vagrant-user':
-		command => "mysql -uroot -prootpwd -e \"GRANT ALL PRIVILEGES ON *.* TO webdev@localhost IDENTIFIED BY 'webdev';\"",
-		path    => ['/usr/bin'],
-		require => Exec['set-mysql-password'],
-	}
-
-	service { 'mysql':
-		ensure     => running,
-		enable     => true,
-		hasrestart => true,
-		hasstatus  => true,
-		require    => [
-			Package['mysql'],
-		],
-
-	}
+    }
 }
