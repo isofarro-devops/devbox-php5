d6e3c41bd0f0e03a1c3f403c65030880e30efc06
Mike Davies <isofarro2@gmail.com>
2015-02-10 15:13:37 +0000

    Fix packages to work with puppet > 3.4.0

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -2,22 +2,21 @@
 # Base install
 #
 class baseconfig {
-	exec { 'apt-update':
-		command => 'apt-get update',
-		user    => 'root',
-	}
+    exec { 'apt-update':
+        command => 'apt-get update',
+        user    => 'root',
+    }
 
-	package { 'core-packages':
-		name => [
-			'python-software-properties',
-			'wget',
-			'curl',
-			'vim',
-			'git-core',
-			'htop',
-			'sqlite3',
-		],
-		ensure  => installed,
-		require => Exec['apt-update'],
-	}
+    package { [
+            'python-software-properties',
+            'wget',
+            'curl',
+            'vim',
+            'git-core',
+            'htop',
+            'sqlite3',
+        ]:
+        ensure  => present,
+        require => Exec['apt-update'],
+    }
 }
