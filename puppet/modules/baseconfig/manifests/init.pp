bf9239634536df179933fbd27d92ea4613ccc049
Mike Davies <isofarro2@gmail.com>
2016-05-28 13:40:52 +0100

    add apt-get update to the baseline

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -7,6 +7,14 @@ class baseconfig {
         user    => 'root',
     }
 
+    exec { "apt-upgrade":
+        command => "/usr/bin/apt-get --yes --fix-broken upgrade",
+        logoutput => "on_failure",
+        timeout => 0,
+        path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:/sbin",
+        require => Exec['apt-update'],
+    }
+
     package { [
             'python-software-properties',
             'wget',
@@ -15,7 +23,7 @@ class baseconfig {
             'git-core',
             'htop',
             'sqlite3',
-            'unzip'
+            'unzip',
         ]:
         ensure  => present,
         require => Exec['apt-update'],
